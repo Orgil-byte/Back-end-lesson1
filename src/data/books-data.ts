621a494252e5ccc3b1bdb148eca49dc142ac0ad5
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-03 16:02:30 +0800

    A

---
+++ src/data/books-data.ts
@@ -0,0 +1,17 @@
+export let books = [
+  {
+    id: 1,
+    title: "Hello",
+    author: "Orgil",
+  },
+  {
+    id: 2,
+    title: "Hello 2",
+    author: "Orgil",
+  },
+  {
+    id: 3,
+    title: "Hello 3",
+    author: "Orgil",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-const books = [
+let books = [
   {
     id: 1,
     title: "Hello",
@@ -18,28 +18,25 @@ const books = [
     author: "Orgil",
   },
 ];
+
 const app = express();
-const port = "3000";
+const port = process.env.PORT ?? "3000";
 
-// app.get("/movie/:type", (req: Request, res: Response) => {
-//   //   const params = req.params;
-//   //   const query = req.query;
-//   const headers = req.headers;
-//   const token = "token";
-//   const token1 = headers.authorization?.split(" ").slice(1).toString();
-//   token !== token1
-//     ? res.status(403).send("Token is not right")
-//     : res.status(200).send("Success");
-//   console.log(token1);
-//   //   console.log("params", params);
-//   //   console.log(query);
-//   console.log(headers);
-// });
+const validateBook = (req: Request, res: Response, next: NextFunction) => {
+  const { title, author } = req.body;
+  if (!title || !author) {
+    res.status(400).send("Title and Author is required");
+    return;
+  }
+  next();
+};
 
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
 app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 app.get("/books", (req: Request, res: Response) => {
   res.status(200).send(books);
@@ -49,38 +46,38 @@ app.get("/books/:id", (req: Request, res: Response) => {
   const { id } = req.params;
   const book = books.find((book) => String(book.id) === String(id));
 
+  if (!book) {
+    res.status(404).send({ message: "Book not found" });
+    return;
+  }
+
   res.status(200).send(book);
 });
 
-app.post("/books", (req: Request, res: Response) => {
+app.post("/books", validateBook, (req: Request, res: Response) => {
   const { title, author } = req.body;
-
-  const newBookId = books.length + 1;
-
-  const newBook = { id: newBookId, title, author };
-
+  const newBook = { id: books.length + 1, title, author };
   books.push(newBook);
-  res.send(books);
+  res.status(201).send(books);
 });
 
-app.put("/books/:id", (req: Request, res: Response) => {
+app.put("/books/:id", validateBook, (req: Request, res: Response) => {
   const { id } = req.params;
   const { updateTitle } = req.body;
-  const updatedBook = books.map((book) => {
-    // String(book.id) === id ? { ...book, title: updateTitle } : book;
+  books = books.map((book) => {
     if (String(book.id) === id) {
       return { ...book, title: updateTitle };
     } else {
       return book;
     }
   });
-  res.send(updatedBook);
+  res.send(books);
 });
 
 app.delete("/books/:id", (req: Request, res: Response) => {
   const { id } = req.params;
-  const deleteBook = books.filter((book) => String(book.id) !== String(id));
-  res.status(200).send(deleteBook);
+  books = books.filter((book) => String(book.id) !== String(id));
+  res.status(200).send(books);
 });
 
 app.listen(port, () => {
